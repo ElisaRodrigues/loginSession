bfade0015cf2853684e0f5e6b289f619f83f39fe
ElisaRodrigues <elisabeth.estudos@gmail.com>
2017-06-14 10:59:25 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+    session_start();
+
+    //if(!isset($_SESSION['usuario_online']) OR !($_SESSION['usuario_online'] == true) ){
+     //header('Location: login.php');
+    //}
+
+
+    //OR ESSE:
+
+
+    $logado = isset($_SESSION['usuario_online']);
+         if ($logado == false){
+            header('Location: login.php');
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="social">
+
+    <a href="verifica_usuario.php?acao=sair" class="sair">sair</a>
+
+    <img src="http://images.e-konomista.pt/repo/melhores-praias-portugal-rocha.jpg" alt="" width="200" height="200">
+    <h3>Bem vindo!</h3>
+</div>
+
+</body>
+</html>
